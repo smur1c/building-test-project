a31a98e4da46dee15c4d54cb3130839394413491
smur1c <42814394+smur1c@users.noreply.github.com>
2022-01-14 12:00:08 +0100

    Adjusted first test
    _01_createNewBuildingShallBuildingShallNotBeNullAndIdShallBe1() has been adjusted in order to check whether the id has increased and class Building was also changed (added new variables id and initalresident), created own constructor

--- src/com/company/Building.java
+++ src/com/company/Building.java
@@ -1,5 +1,10 @@
 package com.company;
 
 public class Building {
+    public static Integer id = 0;
+    public String initialResident;
 
+    public Building(){
+        id++;
+    }
 }
--- test/BuildingTestProjectTests.java
+++ test/BuildingTestProjectTests.java
@@ -4,12 +4,11 @@ import org.junit.Test;
 
 
 
-public class BuldingTestProjectTests {
+public class BuildingTestProjectTests {
     @Test
-    public void _01_createNewBuildingShallBuildingShallNotBeNull() {
+    public void _01_createNewBuildingShallBuildingShallNotBeNullAndIdShallBe1() {
         Building building = new Building();
         Assert.assertTrue(building != null);
+        Assert.assertTrue(Building.id == 1);
     }
-
-
 }
